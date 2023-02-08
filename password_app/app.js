25530dd7efa00ae7c001aae44ebd702ff1895129
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-02-08 03:25:19 -0500

    Finishing up section

--- password_app/app.js
+++ password_app/app.js
@@ -2,15 +2,14 @@ const password = prompt("please enter a new password");
 
 // Password must be 6+ characters
 if (password.length >=6) {
-    console.log("LONG ENOUGH PASSWORD")
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!")
+    } else {
+        console.log("Password cannot contain space!")
+    }
 } else {
     console.log("PASSWOrD tOO SHORT! Must be 6+ characters")
 }
 
 //Password cannot include space
 
-if (password.indexOf(' ') === -1) {
-    console.log("Good job! NO space!")
-} else {
-    console.log("Can't put space")
-}
